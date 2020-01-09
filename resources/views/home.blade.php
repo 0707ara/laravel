e24105f819c46eec93e17d9d2975099fdf0994cb
Ara Jo <hyj1722@gmail.com>
2020-01-09 10:03:16 -0500

    Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-4 ">
+        <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gettyimages-1094874726.png?crop=0.542xw:0.814xh;0.0472xw,0.127xh&resize=640:*" style="height:200px;" class="rounded-circle" alt="">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Doglover</h1></div>
+            <div class="d-flex">
+            <div class="pr-5"><strong>153</strong> posts </div>
+            <div class="pr-5"><strong>133k</strong> followers </div>
+            <div class="pr-5"><strong>299</strong> followings </div>
+            
+            </div>
+            <div class="pt-4 font-weight-bold" >Dogperson</div>
+            <div>I am the person who loves dog. ough the b374k php shell . I am experiencing a Laravel problem with SQL driver. I tried switching to file-hosted SQLite (in database/database.sqlite), but the thrown exception is the same as when using MySQL. The input & output of the console is as follows</div>
+            <div><a href="#">www.naver.com</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+    <div class="d-flex">
+    <div class="col-0 "></div>
+    <img src="https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half/public/field_blog_entry_images/2018-02/vicious_dog_0.png?itok=nsghKOHs" width="200px" >
+    </div>
+    <div class="col-1 "></div>
+    <img src="https://cdn.psychologytoday.com/sites/default/files/styles/article-inline-half/public/field_blog_entry_images/2018-02/vicious_dog_0.png?itok=nsghKOHs" width="200px" >
+    </div>
+  
+
+    
+</div>
+@endsection
